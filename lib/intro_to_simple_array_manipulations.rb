ab08b22a2fa282e28c201b8fa2dbef2907046815
Timothy Ragan <timothyragan7@gmail.com>
2019-07-09 20:34:01 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -70,7 +70,7 @@ end
 def using_delete(array, string)
 
 instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-instructors.delete==(instructors,"Steven")
+instructors.delete==instructors,"Steven"
 end
 
 def using_delete_at(array,interger)
